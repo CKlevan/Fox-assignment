5eec5c70cdcec2c2ed8484cffc7985edafd157e6
Corey Klevan <CKlevan@arcadia.edu>
2021-09-27 15:55:40 -0400

    Updated code to fix problems

--- fox/src/fox.java
+++ fox/src/fox.java
@@ -15,20 +15,19 @@ public class fox
     public static int audio;
     public static void main(String[] args) throws FileNotFoundException
     {
-        String message;
+        
         String animalGoesNoise = "";
-        //message will be used for the hidden message
+        // Message will be used for the hidden message
         Scanner sc = new Scanner(new File("fox\\src\\Audio_recordings.txt"));
-        //Scanner reads the audio recording text file
+        // Scanner reads the audio recording text file
         audio = Integer.parseInt(sc.nextLine());
         
         ArrayList<String> sounds = new ArrayList<String>();
         String[] forSounds = null;
-        //Array for all sounds
+        // Array for all sounds
 
         ArrayList<String> knownSounds = new ArrayList<String>();
-        String[] forKnownSounds = null;
-        //Array for sounds we know ex: dog goes woof
+        // Array for sounds we know ex: dog goes woof
 
 
         for(int row = 0; row < audio; row++)
@@ -39,10 +38,9 @@ public class fox
                 sounds.add(word);
             }
             // for (int i = 0; i < forSounds.length; i++)
-            //  sounds.add(forSounds[i]);
+            // sounds.add(forSounds[i]);
         }
-        // sounds.removeAll(knownSounds);
-        //this for loop goes through the text file and finds all the sounds and puts them into a list
+        // this for loop goes through the text file and finds all the sounds and puts them into a list
 
         while(true)
         {
@@ -59,8 +57,8 @@ public class fox
 
         sounds.removeAll(knownSounds);
         System.out.println(sounds);
-        //prints out the hidden message
-        //array is edited to reflect the new code
+        // Prints out the hidden message
+        // Array is edited to reflect the new code
     }
 }
-//"the best ideas come as jokes make your think as funny as possible" - David Ogilvygit
+// "the best ideas come as jokes make your think as funny as possible" - David Ogilvygit
